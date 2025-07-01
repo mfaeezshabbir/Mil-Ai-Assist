c4582493ca63435f09b926955b34934405290d90
Muhammad Faeez Shabbir <faizytech1st@gmail.com>
2025-07-01 06:48:29 +0000

    remove the thing on double click it actually zooms in to the map because

--- src/components/map-view.tsx
+++ src/components/map-view.tsx
@@ -56,6 +56,7 @@ export function MapView({ symbols, onMapDoubleClick }: MapViewProps) {
         }}
         mapStyle="mapbox://styles/mapbox/satellite-streets-v12"
         onDblClick={handleDblClick}
+        doubleClickZoom={false}
       >
         <Geocoder mapboxAccessToken={MAPBOX_TOKEN} mapRef={mapRef} />
         <NavigationControl position="top-right" />
